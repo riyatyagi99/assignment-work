4d0770c12b80e30e3dd638fddaf2da855a028b9c
riyatyagi99 <tyagi.riyaa2022@gmail.com>
2023-06-15 13:42:44 +0530

    snackbar

---
+++ blog/lib/dropdown.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'appbar.dart';
+import 'common_appbar.dart';
+
+class DropdownExample extends StatefulWidget {
+  const DropdownExample({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownExample> createState() => _DropdownExampleState();
+}
+
+class _DropdownExampleState extends State<DropdownExample> {
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CommonAppBar(
+        toolbarHeight: 50,
+        appBar: AppBar(),
+        title: const Text("Custom Appbar"),
+        centreTitle: false,
+        bgColor: Colors.black38,
+        actions: const [
+          Icon(Icons.person)
+        ],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+         crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>FlutterAppbar()));
+                },
+                child: Text("Hello",style: TextStyle(fontSize: 30,color: Colors.red),))
+          ],
+        ),
+      )
+    );
+  }
+}
